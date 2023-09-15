9b9e96e11bd359258d70647f1f5800ae3a717638
bishwaranjanroutray <105266189+bishwaranjanroutray@users.noreply.github.com>
2023-09-15 12:05:08 +0530

    Add files via upload

---
+++ difference_of_ascii_value.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int r,h;
+    char a,b;
+    cout<<"ENTER THE FIRST CHARACTER:\n";
+    cin>>a;
+    cout<<"ENTER THE SECOND CHARACTER:\n";
+    cin>>b;
+    r=(int)a;
+    h=(int)b;
+    cout<<"THE DIFFERENCE OF ASCII VALUE OF TWO CHARACTER IS:"<<r-h;
+
+}
---
+++ volume.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int r,h;
+    cout<<"ENTER THE VALUE FOR RADIUS AND HEIGHT:";
+    cin>>r>>h;
+    cout<<3.14*r*h;
+
+}
